7669e7984115b0960aaf2ff7368476046691e6a1
Luís Amílcar Tavares <mikusher@hotmail.com>
2020-08-07 16:28:31 +0100

    Version 1, Shark Zip application.

---
+++ src/principal/DefaultToRemove.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author LuisT
+ */
+public class DefaultToRemove {
+
+
+    /**
+     * @param exclusions
+     * @return
+     */
+    public static List<String> mergeToDefaultList(List<String> exclusions) {
+
+        exclusions.removeAll(Arrays.asList("", null));
+
+        List<String> standardList = new ArrayList<>();
+        //others
+        standardList.add("*.cbl");
+        standardList.add("*.ec");
+        standardList.add("*Test.*");
+        standardList.add("**/*~");
+        standardList.add("**/#*#");
+        standardList.add("**/.#*");
+        standardList.add("**/%*%");
+        standardList.add("**/._*");
+        //fonts
+        standardList.add("*.eot");
+        standardList.add("*.ttf");
+        standardList.add("*.woff");
+        standardList.add("*.woff2");
+        // Visual SourceSafe
+        standardList.add("**/vssver.scc");
+        // Subversion
+        standardList.add("**/.svn");
+        standardList.add("**/.svn/**");
+        standardList.add("*.gitignore");
+        standardList.add("*.dockerignore");
+        // Mac
+        standardList.add("**/.DS_Store");
+        //exec fils and compile
+        standardList.add("*.exe");
+        standardList.add("*.msi");
+        standardList.add("*.cmd");
+        standardList.add("*.dll");
+        standardList.add("*.bat");
+        standardList.add("*.sln");
+        standardList.add("*.md");
+        standardList.add("*.csproj");
+        standardList.add("*.cache");
+        // js/css minifile
+        standardList.add("*.min.js");
+        standardList.add("*.min.js.map");
+        standardList.add("*jquery.js");
+        standardList.add("*.bootstrap.js");
+        standardList.add("*jquery.min.map");
+        standardList.add("*.css");
+        standardList.add("*.min.css");
+        standardList.add("*.min.css.map");
+        //files
+        standardList.add("*.jpeg");
+        standardList.add("*.png");
+        standardList.add("*.bmp");
+        standardList.add("*.ico");
+        standardList.add("*.svg");
+        standardList.add("*.mp3");
+        standardList.add("*.mp4");
+        standardList.add("*.webm");
+        standardList.add("*.avi");
+        standardList.add("*.zip");
+        standardList.add("*.rar");
+        standardList.add("*.7z");
+        standardList.add("*.pdf");
+        standardList.add("*.iso");
+        standardList.add("*.jar");
+        standardList.add("*.apk");
+        standardList.add("*.yml");
+        //documentation
+        standardList.add("*.docx");
+        standardList.add("*.doc");
+        standardList.add("*.xlsx");
+        standardList.add("*.csv");
+        //database file
+        standardList.add("*.db");
+        standardList.add("*.sqlite");
+        standardList.add("*.dylib");
+
+
+        return new ArrayList<String>() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
+
+            {
+                addAll(exclusions);
+                addAll(standardList);
+            }
+        };
+
+    }
+
+    ;
+
+}
---
+++ src/principal/ZipCompress.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ * @author LuisT
+ */
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.tools.ant.DirectoryScanner;
+
+public class ZipCompress {
+
+    static int count = 0;
+
+    public static int compressRx(String dirPath, List<String> exclusions, boolean applyDefault) {
+        final Path sourceDir = Paths.get(dirPath);
+        String zipFileName = dirPath.concat(".zip");
+
+
+        List<String> finalExclusionList = (applyDefault) ? DefaultToRemove.mergeToDefaultList(exclusions) : exclusions;
+
+        System.out.println("Exclusion Applied:\n" + finalExclusionList.toString() + "\n");
+
+        File[] fRm = filesToExcluding(sourceDir.toString(), finalExclusionList);
+        List<File> filesToRemove = Arrays.asList(fRm);
+
+        try {
+
+            final ZipOutputStream outputStream = new ZipOutputStream(new FileOutputStream(zipFileName));
+
+            Files.walkFileTree(sourceDir, new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
+
+                    if (filesToRemove.contains(file.toFile())) {
+                        count++;
+                        System.out.println("File removed: " + file.toString());
+                    } else {
+                        try {
+                            Path targetFile = sourceDir.relativize(file);
+                            outputStream.putNextEntry(new ZipEntry(targetFile.toString()));
+                            byte[] bytes = Files.readAllBytes(file);
+                            outputStream.write(bytes, 0, bytes.length);
+                            outputStream.closeEntry();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return FileVisitResult.CONTINUE;
+                    }
+
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
+    private static File[] filesToExcluding(final String sourceDir, List<String> exlcusions) {
+        List<String> myList = new ArrayList<>();
+
+        DirectoryScanner scanner = new DirectoryScanner();
+
+        for (Object exlcusion : exlcusions) {
+            myList.add(String.format("**/%s", exlcusion.toString()));
+        }
+
+        String[] arr = myList.toArray(new String[myList.size()]);
+        scanner.setIncludes(arr);
+
+        scanner.setBasedir(sourceDir);
+        scanner.setCaseSensitive(false);
+        scanner.scan();
+
+        String[] allPaths = scanner.getIncludedFiles();
+        List<File> allFiles = new ArrayList<>();
+        String[] directories = scanner.getIncludedDirectories();
+        for (String directory : directories) {
+            allFiles.add(new File(sourceDir, directory));
+        }
+
+        for (int i = 0; i < allPaths.length; i++) {
+            File file = new File(sourceDir, allPaths[i]);
+            if (!allFiles.contains(file.getParentFile())) {
+                allFiles.add(file);
+            }
+        }
+        return allFiles.toArray(new File[]{});
+    }
+}
